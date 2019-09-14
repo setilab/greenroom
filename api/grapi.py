9b6e7ac7d9763850b06019e049b9c902d1a4fac8
setilab <39970994+setilab@users.noreply.github.com>
2019-09-14 01:04:33 -0600

    Update grapi.py

--- api/grapi.py
+++ api/grapi.py
@@ -8,13 +8,13 @@ import redis
 
 _VERSION_ = "1.5.1"
 
-print sys.argv
+print(sys.argv)
 args = sys.argv[1].split(":")
-print args
+print(args)
 _RHOST_ = args[0]
 _RPORT_ = args[1]
-print _RHOST_
-print _RPORT_
+print(_RHOST_)
+print(_RPORT_)
 
 sys.argv[1] = ""
 
